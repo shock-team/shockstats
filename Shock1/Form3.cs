a06239bf585b0696fae3ce438f61e9d4818f8632
Santiago Gerhart <gerhartsantiago@gmail.com>
2019-08-21 20:27:54 -0300

    Update Form3.cs

--- Shock1/Form3.cs
+++ Shock1/Form3.cs
@@ -23,10 +23,6 @@ namespace Shock1
             ActiveControl = txtCuartil;
         }
 
-        private void MaterialFlatButton1_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void BtnCalcular_Click(object sender, EventArgs e)
         {
